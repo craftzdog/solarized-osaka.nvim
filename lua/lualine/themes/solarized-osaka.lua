4351a86d6f8abdffe1c6bfe2d26147695afd9db5
tinusgraglin <tinusgraglin@gmail.com>
2024-01-22 15:52:20 +0800

    fix(lualine): lualine b section looks unpleasant status info in b section was set to use light colors but the bg color was also light

--- lua/lualine/themes/solarized-osaka.lua
+++ lua/lualine/themes/solarized-osaka.lua
@@ -5,7 +5,7 @@ local solarized_osaka = {}
 
 solarized_osaka.normal = {
   a = { bg = colors.blue, fg = colors.black },
-  b = { bg = colors.fg, fg = colors.black },
+  b = { bg = colors.bg_statusline, fg = colors.fg },
   c = { bg = colors.bg_statusline, fg = colors.fg },
 }
 
