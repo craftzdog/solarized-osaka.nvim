12126b297a2c727409ada4f03fac42d0d902ae72
Redoxahmii <fullmetal2334@gmail.com>
2024-12-16 18:05:55 +0500

    feat(blink): add blink hl groups

--- lua/solarized-osaka/theme.lua
+++ lua/solarized-osaka/theme.lua
@@ -656,6 +656,65 @@ function M.setup()
     -- LightspeedUniqueChar = { link = "LightspeedUnlabeledMatch" },
     LightspeedUnlabeledMatch = { fg = c.violet500, bold = true },
 
+    --Blink-cmp
+    --INFO: passing bg.none to these causes the menu to be completely invisible not the slightly transparent behavior shown on nvim-cmp
+    -- BlinkCmpMenu = { fg = c.base01, bg = c.none },
+    -- BlinkCmpMenuBorder = { fg = c.base02, bg = c.none },
+    -- BlinkCmpMenuSelection = { fg = c.violet500, bg = c.none },
+    -- BlinkCmpScrollBarThumb = { fg = c.blue, bg = c.none },
+    -- BlinkCmpScrollBarGutter = { fg = c.base02, bg = c.bg },
+
+    BlinkCmpLabel = { fg = c.fg, bg = c.none },
+    BlinkCmpLabelDeprecated = { fg = c.base01, bg = c.none, strikethrough = true },
+    BlinkCmpLabelMatch = { fg = c.violet500, bg = c.none },
+
+    -- Documentation windows
+    BlinkCmpDoc = { fg = c.fg, bg = c.bg_float },
+    BlinkCmpDocBorder = { fg = c.base02, bg = c.bg_float },
+    BlinkCmpGhostText = { fg = c.base01 },
+
+    -- Signature help
+    BlinkCmpSignatureHelp = { fg = c.violet500, bg = c.none },
+    BlinkCmpSignatureHelpBorder = { fg = c.base02, bg = c.none },
+    BlinkCmpSignatureHelpActiveParameter = { fg = c.orange, bg = c.none },
+
+    -- ISSUE: passing c.none causes menu to be invisible
+    -- BlinkCmpMenu = { fg = c.base01, bg = c.none },
+    BlinkCmpMenu = { fg = c.base01, bg = c.base02 },
+
+    BlinkCmpKindDefault = { fg = c.base01, bg = c.none }, -- Default fallback
+
+    BlinkCmpKindCodeium = { fg = c.cyan500, bg = c.none },
+    BlinkCmpKindCopilot = { fg = c.cyan500, bg = c.none },
+    BlinkCmpKindTabNine = { fg = c.cyan500, bg = c.none },
+    BlinkCmpKindSupermaven = { fg = c.cyan500, bg = c.none },
+
+    BlinkCmpKindKeyword = { fg = c.cyan, bg = c.none },
+    BlinkCmpKindVariable = { fg = c.magenta, bg = c.none },
+    BlinkCmpKindConstant = { fg = c.magenta, bg = c.none },
+    BlinkCmpKindReference = { fg = c.magenta, bg = c.none },
+    BlinkCmpKindValue = { fg = c.magenta, bg = c.none },
+
+    BlinkCmpKindFunction = { fg = c.blue, bg = c.none },
+    BlinkCmpKindMethod = { fg = c.blue, bg = c.none },
+    BlinkCmpKindConstructor = { fg = c.blue, bg = c.none },
+
+    BlinkCmpKindClass = { fg = c.orange, bg = c.none },
+    BlinkCmpKindInterface = { fg = c.orange, bg = c.none },
+    BlinkCmpKindStruct = { fg = c.orange, bg = c.none },
+    BlinkCmpKindEvent = { fg = c.orange, bg = c.none },
+    BlinkCmpKindEnum = { fg = c.orange, bg = c.none },
+    BlinkCmpKindUnit = { fg = c.orange, bg = c.none },
+
+    BlinkCmpKindModule = { fg = c.yellow, bg = c.none },
+
+    BlinkCmpKindProperty = { fg = c.cyan, bg = c.none },
+    BlinkCmpKindField = { fg = c.cyan, bg = c.none },
+    BlinkCmpKindTypeParameter = { fg = c.cyan, bg = c.none },
+    BlinkCmpKindEnumMember = { fg = c.cyan, bg = c.none },
+    BlinkCmpKindOperator = { fg = c.cyan, bg = c.none },
+    BlinkCmpKindSnippet = { fg = c.violet500, bg = c.none },
+
     -- Cmp
     CmpDocumentation = { fg = c.fg, bg = c.bg_float },
     CmpDocumentationBorder = { fg = c.base02, bg = c.bg_float },
@@ -673,6 +732,7 @@ function M.setup()
     CmpItemKindCodeium = { fg = c.cyan500, bg = c.none },
     CmpItemKindCopilot = { fg = c.cyan500, bg = c.none },
     CmpItemKindTabNine = { fg = c.cyan500, bg = c.none },
+    CmpItemKindSupermaven = { fg = c.cyan500, bg = c.none },
 
     CmpItemKindKeyword = { fg = c.cyan, bg = c.none },
     CmpItemKindVariable = { fg = c.magenta, bg = c.none },
