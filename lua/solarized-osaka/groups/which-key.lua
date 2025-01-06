ab3d30c9aa52d01904bcdae1199064df1f07c74f
Redoxahmii <fullmetal2334@gmail.com>
2025-01-07 03:15:31 +0500

    fix(which-key): add WhichKeySeparator group

--- lua/solarized-osaka/groups/which-key.lua
+++ lua/solarized-osaka/groups/which-key.lua
@@ -6,6 +6,7 @@ function M.get(c, opts)
     WhichKey          = { fg = c.cyan },
     WhichKeyGroup     = { fg = c.blue },
     WhichKeyDesc      = { fg = c.magenta },
+    WhichKeySeperator = { fg = c.base01 },
     WhichKeyBorder    = { fg = c.base02, bg = c.bg_float },
     WhichKeyValue     = { fg = c.violet500 },
   }
